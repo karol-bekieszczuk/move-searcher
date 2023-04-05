a56f3d509301a5c3ee3f58710ae01639489a1228
admin <test@test.com>
2023-04-05 13:45:51 +0200

    updated db/init.sql file

--- db/init.sql
+++ db/init.sql
@@ -1,9 +1,10 @@
 CREATE DATABASE movies;
 
 \c movies;
+CREATE SEQUENCE movie_id_seq START 1;
 
 CREATE TABLE movie (
-    id SERIAL PRIMARY KEY,
+    id INTEGER DEFAULT nextval('movie_id_seq') PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     director VARCHAR(255) NOT NULL,
     genre VARCHAR(255) NOT NULL,
@@ -14,21 +15,10 @@ CREATE USER movies_user WITH ENCRYPTED PASSWORD 'postgres';
 
 GRANT SELECT, INSERT, UPDATE, DELETE ON movie TO movies_user;
 
-INSERT INTO movie (id, title, director, genre, release_date) VALUES
-(1, 'LOTR', 'George lucas lol', 'fantasy', '2016-02-02');
---(2, 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', 'Martin Fowler'),
---(3, 'First, solve the problem. Then, write the code.', 'John Johnson'),
---(4, 'Java is to JavaScript what car is to Carpet.', 'Chris Heilmann'),
---(5, 'Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.', 'John Woods'),
---(6, 'I''m not a great programmer; I''m just a good programmer with great habits.', 'Kent Beck'),
---(7, 'Truth can only be found in one place: the code.', 'Robert C. Martin'),
---(8, 'If you have to spend effort looking at a fragment of code and figuring out what it''s doing, then you should extract it into a function and name the function after the "what".', 'Martin Fowler'),
---(9, 'The real problem is that programmers have spent far too much time worrying about efficiency in the wrong places and at the wrong times; premature optimization is the root of all evil (or at least most of it) in programming.', 'Donald Knuth'),
---(10, 'SQL, Lisp, and Haskell are the only programming languages that I’ve seen where one spends more time thinking than typing.', 'Philip Greenspun'),
---(11, 'Deleted code is debugged code.', 'Jeff Sickel'),
---(12, 'There are two ways of constructing a software design: One way is to make it so simple that there are obviously no deficiencies and the other way is to make it so complicated that there are no obvious deficiencies.', 'C.A.R. Hoare'),
---(13, 'Simplicity is prerequisite for reliability.', 'Edsger W. Dijkstra'),
---(14, 'There are only two hard things in Computer Science: cache invalidation and naming things.', 'Phil Karlton'),
---(15, 'Measuring programming progress by lines of code is like measuring aircraft building progress by weight.', 'Bill Gates'),
---(16, 'Controlling complexity is the essence of computer programming.', 'Brian Kernighan'),
---(17, 'The only way to learn a new programming language is by writing programs in it.', 'Dennis Ritchie');
+INSERT INTO movie (title, director, genre, release_date) VALUES
+('The Lord of the Rings: The Fellowship of the Ring', 'Peter Jackson', 'action fantasy adventure', '2002-02-15'),
+('The Lord of the Rings: The Two Towers', 'Peter Jackson', 'action fantasy adventure', '2003-01-31'),
+('The Lord of the Rings: The Return of the King', 'Peter Jackson', 'action fantasy adventure', '2004-01-01'),
+('Pulp Fiction', 'Quentin Tarantino', 'action thriller mafia comedy dark_comedy crime', '1995-05-19'),
+('Django Unchained', 'Quentin Tarantino', 'spaghetti_western adventure drama exploitation', '1995-05-19'),
+('Titanic', 'James Cameron', 'drama melodrama romance disaster', '1998-02-13');
